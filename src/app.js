b15736b42eefdf94105b14883c85698aa0472596
Krisjward5 <169105340+Krisjward5@users.noreply.github.com>
2024-07-15 23:56:16 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const suit = [
+  ["♦", "diamond"],
+  ["♥", "heart"],
+  ["♠", "spade"],
+  ["♣", "club"]
+];
+
+const face = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suitIndex = Math.floor(Math.random() * suit.length);
+  const randomSuit = suit[suitIndex];
+
+  const topLeftSuitDivElement = document.querySelector(".top-left-suit");
+  topLeftSuitDivElement.classList.add(randomSuit[1]);
+  topLeftSuitDivElement.innerText = randomSuit[0];
+
+  const bottomRightSuitDivElement = document.querySelector(
+    ".bottom-right-suit"
+  );
+  bottomRightSuitDivElement.classList.add(randomSuit[1]);
+  bottomRightSuitDivElement.innerText = randomSuit[0];
+
+  const faceIndex = Math.floor(Math.random() * face.length);
+  const randomFace = face[faceIndex];
+
+  const cardNumberDivElement = document.querySelector(".card-number");
+  cardNumberDivElement.innerText = randomFace;
 };
